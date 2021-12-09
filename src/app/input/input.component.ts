a832bb31499c64d01d97b2fffcdc6c303ed2f2c1
Marcos Ochieng <marcosgav80@gmail.com>
2021-12-09 23:20:14 +0300

    added the add task functionality

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -7,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InputComponent implements OnInit {
 
+  // add task
+  list: any[] = [];
+  addTask(item:string){
+  this.list.push({id:this.list.length,name:item});
+  }
+
+  // remove task
+  removeTask(id:number){
+    this.list = this.list.filter(item=>(item.id != id));
+  }
+
   constructor() { }
 
   ngOnInit(): void {
